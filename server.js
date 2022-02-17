fef902dd0d04bb2cce120afbbc895bb5ff84ad54
Frank Fang <frankfr@email.unc.edu>
2022-02-17 15:48:41 -0500

    l

--- server.js
+++ server.js
@@ -6,3 +6,8 @@ var port = 5000
 const server = app.listen(port, () => {
     console.log('App is running on port %PORT%'.replace('%PORT%', port));
 })
+
+app.use(function(req, res){
+    res.status(404).send("Endpoint does not exist")
+    res.type("text/plain")
+})
